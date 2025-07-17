0bacea3bbbc0f2f653a75618d83c6859937a31a6
dhruvkumar2506 <145141354+dhruvkumar2506@users.noreply.github.com>
2025-07-17 21:31:42 +0530

    Add .next/static/chunks/src_app_personal-ai_page_tsx_d709400e._.js 1752768099811

---
+++ .next/static/chunks/src_app_personal-ai_page_tsx_d709400e._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_personal-ai_page_tsx_b5919647._.js",
+  "static/chunks/node_modules_next_dist_b93b88b6._.js"
+],
+    source: "dynamic"
+});
